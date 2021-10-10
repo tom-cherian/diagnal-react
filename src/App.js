a44555bfdb4a952c021169d48a3d003158124dbe
Tom Cherian <tom.cherian@experionglobal.com>
2021-10-11 00:16:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import "./App.css";
+import Header from "./components/Header/Header.component";
+import PosterListingPage from "./components/PosterListingPage/PosterListingPage.component";
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <PosterListingPage />
+      </div>
+    );
+  }
 }
 
 export default App;
